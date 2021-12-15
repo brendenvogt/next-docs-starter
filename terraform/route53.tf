63a5842677770db116ad421a1e0454cc075ff48c
Brenden Vogt <bvogtdev@gmail.com>
2021-12-15 03:06:07 -0800

    Separated out the TF files to their respective responsibilities

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,51 +1,51 @@
 
-# # ===== Route53 =====
+# ===== Route53 =====
 
-# # # Provides details about the zone
-# data "aws_route53_zone" "main" {
-#   name         = var.website_domain_main
-#   private_zone = false
-# }
+# # Provides details about the zone
+data "aws_route53_zone" "main" {
+  name         = var.website_domain_main
+  private_zone = false
+}
 
-# # # Creates the DNS record to point on the main CloudFront distribution ID
-# resource "aws_route53_record" "website_cdn_root_record" {
-#   zone_id = data.aws_route53_zone.main.zone_id
-#   name    = var.website_domain_main
-#   type    = "A"
+# # Creates the DNS record to point on the main CloudFront distribution ID
+resource "aws_route53_record" "website_cdn_root_record" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.website_domain_main
+  type    = "A"
 
-#   alias {
-#     name                   = aws_cloudfront_distribution.website_cdn_root.domain_name
-#     zone_id                = aws_cloudfront_distribution.website_cdn_root.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name                   = aws_cloudfront_distribution.website_cdn_root.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn_root.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 
-# # # Creates the DNS record to point on the CloudFront distribution ID that handles the redirection website
-# resource "aws_route53_record" "website_cdn_redirect_record" {
-#   zone_id = data.aws_route53_zone.main.zone_id
-#   name    = var.website_domain_redirect
-#   type    = "A"
+# # Creates the DNS record to point on the CloudFront distribution ID that handles the redirection website
+resource "aws_route53_record" "website_cdn_redirect_record" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.website_domain_redirect
+  type    = "A"
 
-#   alias {
-#     name                   = aws_cloudfront_distribution.website_cdn_redirect.domain_name
-#     zone_id                = aws_cloudfront_distribution.website_cdn_redirect.hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
+  alias {
+    name                   = aws_cloudfront_distribution.website_cdn_redirect.domain_name
+    zone_id                = aws_cloudfront_distribution.website_cdn_redirect.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
 
-# # # Validates the ACM wildcard by creating a Route53 record (as `validation_method` is set to `DNS` in the aws_acm_certificate resource)
-# resource "aws_route53_record" "wildcard_validation" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.wildcard_website.domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
-#   name            = each.value.name
-#   type            = each.value.type
-#   zone_id         = data.aws_route53_zone.main.zone_id
-#   records         = [each.value.record]
-#   allow_overwrite = true
-#   ttl             = "60"
-# }
+# # Validates the ACM wildcard by creating a Route53 record (as `validation_method` is set to `DNS` in the aws_acm_certificate resource)
+resource "aws_route53_record" "wildcard_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.wildcard_website.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  name            = each.value.name
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.main.zone_id
+  records         = [each.value.record]
+  allow_overwrite = true
+  ttl             = "60"
+}
