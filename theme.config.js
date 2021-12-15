e7b2d1012d99ddb2264bd940eaafb14ee6ddafd8
Brenden Vogt <bvogtdev@gmail.com>
2021-12-15 01:35:13 -0800

    Added initial files

---
+++ theme.config.js
@@ -0,0 +1,29 @@
+// theme.config.js
+export default {
+  github: "https://github.com/brendenvogt",
+  projectLink: "https://github.com/brendenvogt/next-docs-starter",
+  docsRepositoryBase:
+    "https://github.com/brendenvogt/next-docs-starter/tree/master",
+  titleSuffix: " – Sample Docs Site",
+  nextLinks: true,
+  prevLinks: true,
+  search: true,
+  customSearch: null,
+  darkMode: true,
+  footer: true,
+  footerText: `${new Date().getFullYear()} © Sample Docs Site`,
+  footerEditLink: `Edit this page on GitHub`,
+  logo: (
+    <>
+      <svg>...</svg>
+      <span>Test Docs</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="description" content="Sample Docs Site" />
+      <meta name="og:title" content="Sample Docs Site" />
+    </>
+  ),
+};
